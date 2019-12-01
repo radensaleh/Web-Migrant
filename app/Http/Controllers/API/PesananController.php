3c868a3503bf881150bfcd4d9272be12f54342ac
fiqrimfr2729 <madridistafiqri@gmail.com>
2019-12-01 15:07:20 +0700

    update transaski

--- app/Http/Controllers/API/PesananController.php
+++ app/Http/Controllers/API/PesananController.php
@@ -65,11 +65,10 @@ class PesananController extends Controller
         $kd_user = $request->kd_user;
         $nama_penerima = $request->nama_penerima;
         $city_id = $request->city_id;
-        //$ongkirs = $request->ongkir;
-        $ongkirs = [0,1,2,3];
+        $ongkirs = $request->ongkir;
         $est_pengiriman = $request->estimasi_pengiriman;
         $kurir = $request->kurir;
-        $nama_service[] = $request->nama_service;
+        $nama_service = $request->nama_service;
         $alamatLengkap = $request->alamat_lengkap;
         $no_hp = $request->nomor_hp;
         //
@@ -77,109 +76,108 @@ class PesananController extends Controller
         $tgl = str_replace('-','', $getDate);
         $jam = str_replace(':','', $tgl);
         $kd_transaksi = 'TRX'.str_replace(' ','',$jam);
+        //Data dari Keranjang untuk dimasukkan ke pesanan dan listBarang
+        $keranjang = Keranjang::where('kd_user', $kd_user)->get();
+        $total_harga_pesanan = 0;
+        $total_ongkir = 0;
+        $total_harga_all_pesanan = 0;
+        $comission_fee = 0;
 
-        return response()->json([
-                 'response' => true,
-                 'message' => $est_pengiriman,
-                 
-                 'bank' => $ongkirs
-             ]);
+        //Create Transaksi
+        $dataTransaksi = array(
+            'kd_transaksi' => $kd_transaksi,
+            'kd_user' => $kd_user,
+            'nama_penerima' => $nama_penerima
+        );
+        Transaksi::create($dataTransaksi);
+        for($j=0; $j<sizeof($keranjang); $j++) {
+            $getDate = Carbon::now('Asia/Jakarta');
+            $tgl = str_replace('-','', $getDate);
+            $jam = str_replace(':','', $tgl);
+            $kd_pesanan = 'PSN'.str_replace(' ','',$jam).$j;
+            //GetListBarangKeranjang
+            $listBarangKeranjang = ListBarangKeranjang::where('id_keranjang', $keranjang[$j]->id_keranjang)->get();
+            //CreatePesanan
+            $dataPesanan = array (
+                'kd_pesanan' => $kd_pesanan,
+                'kd_transaksi' => $kd_transaksi,
+                'city_id' => $city_id,
+                'id_status' => 1,
+            );
+            Pesanan::create($dataPesanan);
+            //Input Ke listBarang
+            for($i=0; $i<sizeof($listBarangKeranjang); $i++) {
+                $dataBarang = array(
+                    'kd_pesanan' => $kd_pesanan,
+                    'kd_barang' => $listBarangKeranjang[$i]->kd_barang,
+                    'kuantitas' => $listBarangKeranjang[$i]->kuantitas,
+                    'harga' => $listBarangKeranjang[$i]->harga
+                );
+                $total_harga_pesanan += $listBarangKeranjang[$i]->harga*$listBarangKeranjang[$i]->kuantitas;
+                ListBarang::create($dataBarang);
+                //Update Stok Barang
+                // $barang = Barang::where('kd_barang',$listBarangKeranjang[$i]->kd_barang)->first();
+                // $stokBarang = $barang->stok;
+                // $updateStok = array (
+                //     'stok' => $stokBarang-$listBarangKeranjang[$i]->kuantitas);
+                // $updateBarang = Barang::findOrFail($listBarangKeranjang[$i]->kd_barang);
+                // $updateBarang->update($updateStok);
 
-
-        // //Data dari Keranjang untuk dimasukkan ke pesanan dan listBarang
-        // $keranjang = Keranjang::where('kd_user', $kd_user)->get();
-        // $total_harga_pesanan = 0;
-        // $total_ongkir = 0;
-        // $total_harga_all_pesanan = 0;
-        // $comission_fee = 0;
-        // //Create Transaksi
-        // $dataTransaksi = array(
-        //     'kd_transaksi' => $kd_transaksi,
-        //     'kd_user' => $kd_user,
-        //     'nama_penerima' => $nama_penerima
-        // );
-        // Transaksi::create($dataTransaksi);
-        // for($j=0; $j<sizeof($keranjang); $j++) {
-        //     $getDate = Carbon::now('Asia/Jakarta');
-        //     $tgl = str_replace('-','', $getDate);
-        //     $jam = str_replace(':','', $tgl);
-        //     $kd_pesanan = 'PSN'.str_replace(' ','',$jam).$j;
-        //     //GetListBarangKeranjang
-        //     $listBarangKeranjang = ListBarangKeranjang::where('id_keranjang', $keranjang[$j]->id_keranjang)->get();
-        //     //CreatePesanan
-        //     $dataPesanan = array (
-        //         'kd_pesanan' => $kd_pesanan,
-        //         'kd_transaksi' => $kd_transaksi,
-        //         'city_id' => $city_id,
-        //         'id_status' => 1,
-        //     );
-        //     Pesanan::create($dataPesanan);
-        //     //Input Ke listBarang
-        //     for($i=0; $i<sizeof($listBarangKeranjang); $i++) {
-        //         $dataBarang = array(
-        //             'kd_pesanan' => $kd_pesanan,
-        //             'kd_barang' => $listBarangKeranjang[$i]->kd_barang,
-        //             'kuantitas' => $listBarangKeranjang[$i]->kuantitas,
-        //             'harga' => $listBarangKeranjang[$i]->harga
-        //         );
-        //         $total_harga_pesanan += $listBarangKeranjang[$i]->harga*$listBarangKeranjang[$i]->kuantitas;
-        //         ListBarang::create($dataBarang);
-        //         //Update Stok Barang
-        //         $barang = Barang::where('kd_barang',$listBarangKeranjang[$i]->kd_barang)->first();
-        //         $stokBarang = $barang->stok;
-        //         $updateStok = array (
-        //             'stok' => $stokBarang-$listBarangKeranjang[$i]->kuantitas);
-        //         $updateBarang = Barang::findOrFail($listBarangKeranjang[$i]->kd_barang);
-        //         $updateBarang->update($updateStok);
-        //     } //End For 2
-        //     $total_harga_all_pesanan += $total_harga_pesanan;
-        //     $pesanan = [
-        //         'kd_pesanan' => $kd_pesanan,
-        //         'kd_transaksi' => $kd_transaksi,
-        //         //Total Harga Pesanan
-        //         'total_harga' => $total_harga_pesanan,
-        //         'ongkir' => $ongkirs[$j],
-        //         'city_id' => $city_id,
-        //         'id_status' => 1,
-        //         'estimasi_pengiriman' => $est_pengiriman[$j],
-        //         'kurir' => $kurir,
-        //         'nama_service' => $nama_service[$j],
-        //         'alamat_lengkap' => $alamatLengkap,
-        //         'nomor_hp' => $no_hp
-        //     ];
-        //     $total_ongkir += $ongkirs[$j];
-        //     $updatePesanan = Pesanan::findOrFail($kd_pesanan);
-        //     $updatePesanan->update($pesanan);
-        //     $total_harga_pesanan = 0;
-        // } //End For 1
-        // $comission_fee = ($total_harga_all_pesanan + $total_ongkir) * 5 / 100;
-        // $transaksi = [
-        //     'kd_transaksi' => $kd_transaksi,
-        //     'kd_user' => $kd_user,
-        //     'tgl_transaksi' => Carbon::now('Asia/Jakarta'),
-        //     //Total Harga Transaksi, sudah plus keuntungan migrant
-        //     'total_harga' => $total_harga_all_pesanan + $total_ongkir + $comission_fee,
-        //     'nama_penerima' => $nama_penerima
-        // ];
-        // $updateTransaksi = Transaksi::findOrFail($kd_transaksi);
-        // $bank = Bank::first();
-        // if($updateTransaksi->update($transaksi)) {
-        //     for($i=0; $i<sizeof($keranjang); $i++) {
-        //         DB::table('tb_list_barang_keranjang')->where('id_keranjang', $keranjang[$i]->id_keranjang)->delete();
-        //     }
-        //     DB::table('tb_keranjang')->where('kd_user', $kd_user)->delete();
-        //     return response()->json([
-        //         'response' => true,
-        //         'message' => 'Transaction Successfull',
-        //         'transaksi' => $updateTransaksi,
-        //         'bank' => $bank
-        //     ]);
-        // } else {
-        //     return response()->json([
-        //         'response' => false,
-        //         'message' => 'Transaction Failed !'
-        //     ]);
-        // } //end else
+                $data=array(
+                  'id_produk' => $listBarangKeranjang[$i]->kd_barang,
+                  'qty_produk_keluar' => $listBarangKeranjang[$i]->kuantitas,
+                  'tgl_masuk_keluar' => $getDate
+                );
+                DB::table('produk_keluar')->insert($data);
+            } //End For 2
+            $total_harga_all_pesanan += $total_harga_pesanan;
+            $pesanan = [
+                'kd_pesanan' => $kd_pesanan,
+                'kd_transaksi' => $kd_transaksi,
+                //Total Harga Pesanan
+                'total_harga' => $total_harga_pesanan,
+                'ongkir' => $ongkirs[$j],
+                'city_id' => $city_id,
+                'id_status' => 1,
+                'estimasi_pengiriman' => $est_pengiriman[$j],
+                'kurir' => $kurir,
+                'nama_service' => $nama_service[$j],
+                'alamat_lengkap' => $alamatLengkap,
+                'nomor_hp' => $no_hp
+            ];
+            $total_ongkir += $ongkirs[$j];
+            $updatePesanan = Pesanan::findOrFail($kd_pesanan);
+            $updatePesanan->update($pesanan);
+            $total_harga_pesanan = 0;
+        } //End For 1
+        $comission_fee = floor(($total_harga_all_pesanan + $total_ongkir) * 5 / 100);
+        $transaksi = [
+            'kd_transaksi' => $kd_transaksi,
+            'kd_user' => $kd_user,
+            'tgl_transaksi' => Carbon::now('Asia/Jakarta'),
+            //Total Harga Transaksi, sudah plus keuntungan migrant
+            'total_harga' => $total_harga_all_pesanan + $total_ongkir + $comission_fee,
+            'nama_penerima' => $nama_penerima
+        ];
+        $updateTransaksi = Transaksi::findOrFail($kd_transaksi);
+        $bank = Bank::first();
+        if($updateTransaksi->update($transaksi)) {
+            for($i=0; $i<sizeof($keranjang); $i++) {
+                DB::table('tb_list_barang_keranjang')->where('id_keranjang', $keranjang[$i]->id_keranjang)->delete();
+            }
+            DB::table('tb_keranjang')->where('kd_user', $kd_user)->delete();
+            return response()->json([
+                'response' => true,
+                'message' => 'Transaction Successfull',
+                'transaksi' => $updateTransaksi
+                // 'bank' => $bank
+            ]);
+        } else {
+            return response()->json([
+                'response' => false,
+                'message' => 'Transaction Failed !'
+            ]);
+        } //end else
     }
     /**
      * Store a newly created resource in storage.
@@ -255,7 +253,6 @@ class PesananController extends Controller
         $kd_user = request()->kd_user;
         $toko = Toko::where('kd_user', $kd_user)->first();
         $kd_toko = $toko->kd_toko;
-
         if($toko==null) {
             return response()->json([
                 'response' => false,
@@ -286,7 +283,6 @@ class PesananController extends Controller
             {
                 return PesananTokoResource::collection($pesanan);
             }
-
         }
     }
     /**
@@ -330,7 +326,6 @@ class PesananController extends Controller
         ->whereHas('transaksi', function($query){
             $query->where('kd_user', request('kd_user'));
         })->with(['status','city'])->get();
-
         if(sizeof($pesanan)!=0) {
             return PesananResource::collection($pesanan);
         }
@@ -362,9 +357,7 @@ class PesananController extends Controller
     //kd_pesanan
     {
         $kd_pesanan = request()->kd_pesanan;
-
         $barang = ListBarang::where('kd_pesanan', $kd_pesanan)->with('barang')->get();
-
         if($barang) {
             return ListBarangResource::collection($barang);
         }
